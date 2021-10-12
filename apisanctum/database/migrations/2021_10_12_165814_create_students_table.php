81725c824989b7229c1930738629ce12c8862e76
hseka <hseka@lescollectionneurs.com>
2021-10-12 22:39:36 +0200

    Initialisation de l'API: models, contrôleurs et routes

---
+++ apisanctum/database/migrations/2021_10_12_165814_create_students_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string("name", 250);
+            $table->string("email", 250);
+            $table->string("password", 250);
+            $table->string("phone_no", 25);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
--- apisanctum/routes/api.php
+++ apisanctum/routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\StudentController;
+use App\Http\Controllers\Api\ProjectController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +16,22 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post("register", [StudentController::class, "register"]);
+Route::post("login", [StudentController::class, "login"]);
+
+Route::group(["middleware" => ["auth:sanctum"]], function(){
+
+    Route::get("profile", [StudentController::class, "profile"]);
+    Route::get("logout", [StudentController::class, "logout"]);
+
+    Route::post("create-project", [ProjectController::class, "createProject"]);
+    Route::get("list-project", [ProjectController::class, "listProject"]);
+    Route::get("single-project/{id}", [ProjectController::class, "singleProject"]);
+    Route::delete("delete-project/{id}", [ProjectController::class, "deleteProject"]);
+
+});
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
